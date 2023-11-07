139dd80aaccc0ae2f4661031b09132f5c7f4ba39
howsen82 <howsenpl@hotmail.com>
2023-11-07 12:42:14 +0800

    Initial commit

---
+++ main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo '[+] Initializing local clock'
+ntpdate -B -q 0.debian.pool.ntp.org
+source /usr/local/rvm/scripts/rvm
+echo '[+] Initializing postgres'
+service postgresql start
+echo '[+] Initializing tor'
+service tor start
+
+#echo '[+] Updating msf'
+/opt/msf/msfupdate --git-branch master
+
+echo '[+] Well done, enjoy your shell'
+cd /opt/msf/
+tmux new-session "./msfconsole; read"
